a368ef8ad00bffcd98e7fab6d53093a3e3f0800a
Pablo Toledo Astudillo <pa.toledoa@duocuc.cl>
2022-10-01 12:23:00 -0300

    Intro datepicker y terminado de modelar mantenciones

---
+++ TurismoRealWeb.BLL/Datepicker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TurismoRealWeb.BLL
+{
+    public class Datepicker
+    {
+        public DateTime Datedata { get; set; }
+    }
+}
